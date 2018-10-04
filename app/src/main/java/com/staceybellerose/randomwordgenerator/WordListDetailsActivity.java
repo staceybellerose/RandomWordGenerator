16aa04d882b61663facbb0e6d2643c7eb177dcd7
Stacey Adams <stacey.belle.rose@gmail.com>
2018-10-04 11:46:49 -0500

    Display help text in Word List Details Activity
    Show help text in modal bottom sheet

--- app/src/main/java/com/staceybellerose/randomwordgenerator/WordListDetailsActivity.java
+++ app/src/main/java/com/staceybellerose/randomwordgenerator/WordListDetailsActivity.java
@@ -6,11 +6,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.staceybellerose.randomwordgenerator.adapters.ListDetailsAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import jonathanfinerty.once.Once;
 
 /**
  * Display a list of available word lists to the user and show some stats about each list.
@@ -21,6 +24,10 @@ public class WordListDetailsActivity extends AppCompatActivity {
      * Request code to indicate Word List Details Activity called
      */
     public static final int WORD_LIST_REQUEST_CODE = 314;
+    /**
+     * Key to track when the help fragment has automatically been shown
+     */
+    private static final String ONCE_HELP = "listHelpDisplay";
     /**
      * the toolbar
      */
@@ -43,11 +50,40 @@ public class WordListDetailsActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
         setResult(RESULT_CANCELED);
 
+        if (!Once.beenDone(Once.THIS_APP_INSTALL, ONCE_HELP)) {
+            showHelpFragment();
+            Once.markDone(ONCE_HELP);
+        }
+
         mRecyclerView.setHasFixedSize(true);
         final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(layoutManager);
         final RecyclerView.Adapter adapter = new ListDetailsAdapter(this);
         mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(final MenuItem item) {
+        final int itemId = item.getItemId();
+        if (itemId == R.id.action_help) {
+            showHelpFragment();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Show the Help bottom sheet
+     */
+    private void showHelpFragment() {
+        final WordListHelpFragment helpFragment = WordListHelpFragment.newInstance();
+        helpFragment.show(getSupportFragmentManager(), "help_fragment");
     }
 
 }
---
+++ app/src/main/java/com/staceybellerose/randomwordgenerator/WordListHelpFragment.java
@@ -0,0 +1,78 @@
+package com.staceybellerose.randomwordgenerator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * Fragment to display help text
+ */
+public class WordListHelpFragment extends BottomSheetDialogFragment {
+
+    /**
+     * The Text View displaying the help text
+     */
+    @BindView(R.id.help_text)
+    TextView mTextView;
+    /**
+     * A span which attaches an icon to the Text View
+     */
+    private ImageSpan mImageSpan;
+
+    /**
+     * Create a new instance of the helper fragment
+     *
+     * @return a new fragment
+     */
+    public static WordListHelpFragment newInstance() {
+        return new WordListHelpFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        setStyle(BottomSheetDialogFragment.STYLE_NORMAL, R.style.AppTheme_BottomSheet);
+        mImageSpan = new ImageSpan(getContext(), R.drawable.ic_favorite_green_500_18dp);
+    }
+
+    @Override
+    @SuppressWarnings("PMD.NullAssignment")
+    public void onDestroy() {
+        mImageSpan = null; // prevent context leakage
+        super.onDestroy();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(final LayoutInflater inflater, @Nullable final ViewGroup container,
+                             @Nullable final Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.word_list_help_bottom_sheet, container, false);
+        ButterKnife.bind(this, view);
+        final SpannableString text = new SpannableString(mTextView.getText());
+        final int position = text.toString().indexOf("❤");
+        text.setSpan(mImageSpan, position, position + 1, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        mTextView.setText(text);
+        return view;
+    }
+
+    /**
+     * Click method for help text and header
+     */
+    @SuppressWarnings("unused")
+    @OnClick({R.id.help_text, R.id.help_header})
+    public void onClick() {
+        dismiss();
+    }
+}
